96a8f00409d0d92e7626cbd5474688b9c0048480
João <skweadhunter.final@gmail.com>
2018-11-08 14:28:39 +0000

    continuação do login, mas ta lixado sem Java

--- Programa/WsServerStuff/WsServerStuff/Client.vb
+++ Programa/WsServerStuff/WsServerStuff/Client.vb
@@ -45,9 +45,21 @@ Public Class Client
         Return Encoding.ASCII.GetString(bytes, 0, bytesRec)
     End Function
 
-    Public Sub SendUser(user As String)
+    Public Sub Send(message As String)
 
+        Dim ipHostInfo As IPHostEntry = Dns.GetHostEntry(Dns.GetHostName())
+        Dim ipAddress As IPAddress = ipHostInfo.AddressList(0)
+        Dim remoteEP As New IPEndPoint(ipAddress, 11000)
 
+        Dim sender As New Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp)
+
+        sender.Connect(remoteEP)
+
+        Dim msg As Byte() =
+            Encoding.ASCII.GetBytes(message)
+
+        Dim bytesSent As Integer = sender.Send(msg)
 
     End Sub
 
